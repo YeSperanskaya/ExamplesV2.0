8fef6c11ea3ce86165f3b46166caad9b4ce51514
Evgeniya <speranskaya1991@mail.ru>
2023-09-17 11:07:21 +0500

    add example 005

---
+++ example005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Please, write your name: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "evgeniya")
+{
+    Console.WriteLine("Wow! It is EVGENIYA!");
+}
+else
+{
+    Console.Write("Hello, " + username + "!");
+}
