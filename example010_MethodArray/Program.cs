47ae96626f17939d14bd13afbda3aeaee676f2d8
Evgeniya <speranskaya1991@mail.ru>
2023-09-19 22:51:56 +0500

    added eexample010_MethodArray

---
+++ example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1,21,13,41,15,2,3,2,6,117,8,119,10};
+
+int n = array.Length;
+int find = 2;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index+1;
+    index++;
+}
