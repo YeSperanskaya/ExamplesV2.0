8c837cb6983607a08922ac8a15e7f8fa567022fe
Evgeniya <speranskaya1991@mail.ru>
2023-09-30 10:07:59 +0500

    == нельзя применить к строкам

--- example012_Methods/Program.cs
+++ example012_Methods/Program.cs
@@ -1,29 +1,98 @@
-﻿// Вид 1
-void Method1()
+﻿// // Вид 1
+// void Method1()
+// {
+//     Console.WriteLine("Автор Сперанская Евгения");
+// }
+// //Method1();
+// //Method1();
+
+
+
+// // Вид 2
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// //Method2("Текст сообщения");
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// //Method21(msg:"Text", count: 4);
+// //Method21(count: 4, msg: "новый текст");
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method3();
+// //Console.WriteLine(year);
+
+// // Вид 4
+// // string Method4(int count, string text)
+// // {
+// //     int i = 0;
+// //     string result = String.Empty;
+// //     while (i< count)
+// //     {
+// //         result = result + text;
+// //         i++;
+// //     }
+// //     return result;
+// // }
+
+// // Вид 4
+// string Method4(int count, string text)
+// {
+    
+//     string result = String.Empty;
+//     for(int i = 0; i< count; i++)
+//     {
+//         result = result + text;
+        
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "asdf ");
+// //Console.WriteLine(res);
+
+// for (int i = 2; i<=10; i++)
+// {
+//     for(int j = 2; j<=10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
+//Дан текст. В тексте нужно все пробелы заменить черточками, 
+//маленькие буквы “к” заменить большими “К”, 
+//а большие “С” заменить маленькими “с”.
+string text = "Я думаю, - сказал князь, улыбаясь, что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде, "
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+// string s = "qwerty"
+//             012
+// s[3]  // r
+string Replase(string text, string oldValue, string newValue)
 {
-    Console.WriteLine("Автор Сперанская Евгения");
-}
-//Method1();
-//Method1();
+    string result = String.Empty;
 
-
-
-// Вид 2
-void Method2(string msg)
-{
-    Console.WriteLine(msg);
-}
-//Method2("Текст сообщения");
-
-void Method21(string msg, int count)
-{
-    int i = 0;
-    while (i < count)
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
     {
-        Console.WriteLine(msg);
-        i++;
+        if(text[i] == $"{oldValue}") result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
     }
+    return result;
 }
-//Method21(msg:"Text", count: 4);
-//Method21(count: 4, msg: "новый текст");
+string newText = Replase(text, " ", "|");
+Console.WriteLine(newText);
 
